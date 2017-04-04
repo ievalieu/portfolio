5da077b596bf82f46b1cda3cd81534da3ae14afc
Eva Lieu <itsevalieu@gmail.com>
2017-04-04 03:21:03 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+$(".cross").hide();
+$("section").hide();
+$(".hamburger").click(function() {
+	$("section").slideToggle("fast", function() {
+		$(".hamburger").hide();
+		$(".cross").show();
+	});
+});
+
+$(".cross").click(function() {
+	$("section").slideToggle("fast", function() {
+		$(".cross").hide();
+		$(".hamburger").show();
+	});
+});
